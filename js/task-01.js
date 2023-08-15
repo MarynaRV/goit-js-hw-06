26c15f0cc96649a3e6a2cd03f5cd0d043ef4add5
MarynaRV <130483928+MarynaRV@users.noreply.github.com>
2023-08-15 12:38:38 +0300

    Initial commit.

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const ulItem = document.querySelectorAll(".item");
+console.log(ulItem);
+const ulItemArr = Array.from(ulItem);
+console.log(ulItemArr);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
